264541a9ea8ff53ffaf492f560369464759ba0a4
KalinaMichal <michal230915@gmail.com>
2020-04-21 23:04:46 +0200

    Poprawione na sztorc i okomentowane (lierownki i niespojnosci)

--- Pydate/models.py
+++ Pydate/models.py
@@ -1,51 +1,72 @@
 from django.db import models
 
+####################
+#UŻYTKOWNICY
+####################
+
 class user_static_data(models.Model):
     userID= models.IntegerField(primary_key=True)
     birth = models.DateField(null=False)
-    facbook=models.CharField(max_length=100)
+    facebook=models.CharField(max_length=100)
     instagram=models.CharField(max_length=100)
-    sex=models.IntegerField(null=False)
+    sex=models.CharField(max_length=2,null=False)
     personality=models.IntegerField(null=False)
 
 class user_dynamic_data(models.Model):
-    userID = models.ForeignKey(user_static_data)
-    descrition=models.CharField(max_length=300)
+    userID = models.ForeignKey(user_static_data,on_delete=models.CASCADE)
+    description=models.CharField(max_length=300)
     photo=models.FileField()
     location=models.IntegerField()
     nick=models.CharField(max_length=20,null=False)
 
+####################
+#PYTANIA UŻYTKOWNIKÓW
+####################
+
 class personal_question_content(models.Model):
     questionID= models.IntegerField(primary_key=True)
-    userID=models.ForeignKey(user_static_data)
     content=models.CharField(max_length=250)
 
-class personal_question_users(models.Model):
-    questionID=models.ForeignKey(personal_question_content)
-    userID=models.ForeignKey(user_static_data)
+class personal_question_user(models.Model):
+    questionID=models.ForeignKey(personal_question_content,on_delete=models.CASCADE)
+    userID=models.ForeignKey(user_static_data,on_delete=models.CASCADE)          #ten co pyta
 
-class personal_question_answear(models.Model):
-    userID=models.ForeignKey(user_static_data)
-    questionID=models.ForeignKey(personal_question_content)
+class personal_question_answer(models.Model):
+    userID=models.ForeignKey(user_static_data,on_delete=models.CASCADE)          #ten co odpowiada
+    questionID=models.ForeignKey(personal_question_content,on_delete=models.CASCADE)
     content=models.CharField(max_length=300)
 
-class starter_quetion(models.Model):
+####################
+#PYTANIA STARTOWE
+####################
+
+class starter_question(models.Model):
     questionID= models.IntegerField(primary_key=True)
     content=models.CharField(max_length=250)
 
-class chat(models.Model):
+####################
+#CZAT
+####################
+
+class chat(models.Model):   #id czatu oraz informacja o tym czy czat nie jest zablokowany
     chatID= models.IntegerField(primary_key=True)
-    user1ID=models.ForeignKey(user_static_data)
-    user2ID=models.ForeignKey(user_static_data)
     agreement=models.IntegerField(default=0)
 
-class chat_message(models.Model):
-    chatID=models.ForeignKey(chat)
+class user_chat(models.Model):  #uzytkownicy podlaczeni do danego czatu
+    chatID=models.ForeignKey(chat,on_delete=models.CASCADE)
+    userID=models.ForeignKey(user_static_data,on_delete=models.CASCADE)
+
+class chat_message(models.Model):   #zawartosc czatu
+    chatID=models.ForeignKey(chat,on_delete=models.CASCADE)
     message=models.CharField(max_length=300)
-    date=models.DateField(auto_now=True())
+    date=models.DateField(auto_now=True)
+
+####################
+#STATYSTYKI
+####################
 
 class user_log(models.Model):
-    userID=models.ForeignKey(user_static_data)
+    userID=models.ForeignKey(user_static_data,on_delete=models.CASCADE)
     logins=models.IntegerField(default=1)
     likes_sent=models.IntegerField(default=0)
     likes_receive=models.IntegerField(default=0)
