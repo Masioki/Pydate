3d4e895b143fad51f4b0dbd89a21108391c59143
KalinaMichal <michal230915@gmail.com>
2020-04-24 14:13:32 +0200

    1)Dane o uzytkowniku polaczeone w 1 tabele 2)Usunieta tez tabela z pytaniami pocatkowymi (prosby Jaroslawa) 3)usuniety plik .gitignore(prosba Tomasza)

--- Pydate/models.py
+++ Pydate/models.py
@@ -4,20 +4,18 @@ from django.db import models
 #UŻYTKOWNICY
 ####################
 
-class user_static_data(models.Model):
+class user_data(models.Model):
     userID= models.IntegerField(primary_key=True)
     birth = models.DateField(null=False)
     facebook=models.CharField(max_length=100)
     instagram=models.CharField(max_length=100)
     sex=models.CharField(max_length=2,null=False)
     personality=models.IntegerField(null=False)
-
-class user_dynamic_data(models.Model):
-    userID = models.ForeignKey(user_static_data,on_delete=models.CASCADE)
     description=models.CharField(max_length=300)
     photo=models.FileField()
     location=models.IntegerField()
     nick=models.CharField(max_length=20,null=False)
+
 
 ####################
 #PYTANIA UŻYTKOWNIKÓW
@@ -29,10 +27,10 @@ class personal_question_content(models.Model):
 
 class personal_question_user(models.Model):
     questionID=models.ForeignKey(personal_question_content,on_delete=models.CASCADE)
-    userID=models.ForeignKey(user_static_data,on_delete=models.CASCADE)          #ten co pyta
+    userID=models.ForeignKey(user_data,on_delete=models.CASCADE)          #ten co pyta
 
 class personal_question_answer(models.Model):
-    userID=models.ForeignKey(user_static_data,on_delete=models.CASCADE)          #ten co odpowiada
+    userID=models.ForeignKey(user_data,on_delete=models.CASCADE)          #ten co odpowiada
     questionID=models.ForeignKey(personal_question_content,on_delete=models.CASCADE)
     content=models.CharField(max_length=300)
 
@@ -40,9 +38,8 @@ class personal_question_answer(models.Model):
 #PYTANIA STARTOWE
 ####################
 
-class starter_question(models.Model):
-    questionID= models.IntegerField(primary_key=True)
-    content=models.CharField(max_length=250)
+#TODO: Jarosławie, pisz tu tabele twoją
+
 
 ####################
 #CZAT
@@ -54,7 +51,7 @@ class chat(models.Model):   #id czatu oraz informacja o tym czy czat nie jest za
 
 class user_chat(models.Model):  #uzytkownicy podlaczeni do danego czatu
     chatID=models.ForeignKey(chat,on_delete=models.CASCADE)
-    userID=models.ForeignKey(user_static_data,on_delete=models.CASCADE)
+    userID=models.ForeignKey(user_data,on_delete=models.CASCADE)
 
 class chat_message(models.Model):   #zawartosc czatu
     chatID=models.ForeignKey(chat,on_delete=models.CASCADE)
@@ -66,7 +63,7 @@ class chat_message(models.Model):   #zawartosc czatu
 ####################
 
 class user_log(models.Model):
-    userID=models.ForeignKey(user_static_data,on_delete=models.CASCADE)
+    userID=models.ForeignKey(user_data,on_delete=models.CASCADE)
     logins=models.IntegerField(default=1)
     likes_sent=models.IntegerField(default=0)
     likes_receive=models.IntegerField(default=0)
