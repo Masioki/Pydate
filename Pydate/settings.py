8e82f2cae4f25bffcd3fd8bce08ec624ccf2db04
KalinaMichal <michal230915@gmail.com>
2020-05-24 12:33:38 +0200

    Wyswietlanie zdjec profilowych z lewej strony

--- Pydate/settings.py
+++ Pydate/settings.py
@@ -146,5 +146,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '/static')
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+MEDIA_ROOT = os.path.join(BASE_DIR, "Pydate/static/").replace('\\', '/')
 MEDIA_URL = '/media/'
